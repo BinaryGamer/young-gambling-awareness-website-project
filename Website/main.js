84de1010b0e01d2f008aa3f8d88828e434348202
BinaryGamer <46252418+BinaryGamer@users.noreply.github.com>
2019-04-01 10:06:13 +1100

    Improved Website

--- Website/main.js
+++ Website/main.js
@@ -3,8 +3,20 @@ window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navi").style.top = "0";
+  } else if (currentScrollPos < 80) {
+    document.getElementById("navi").style.top = "0";
   } else {
     document.getElementById("navi").style.top = "-66px";
   }
   prevScrollpos = currentScrollPos;
 }
+
+function toggleFocusOne(self) {
+   self.target.classList.toggle('focus');
+}
+
+document.getElementById("imageOne").onmouseover = toggleFocusOne;
+document.getElementById("imageOne").onmouseleave = toggleFocusOne;
+
+document.getElementById("imageTwo").onmouseover = toggleFocusOne;
+document.getElementById("imageTwo").onmouseleave = toggleFocusOne;
